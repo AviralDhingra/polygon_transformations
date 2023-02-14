14535d0abfdbb4d476b83b52c03761e38244e133
Aviral Dhingra <deathblade287@pop-os.localdomain>
2023-02-14 22:23:50 +0530

    Commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from turtle import speed, bgcolor, colormode, fd, rt, pencolor
+import turtle
+import sys
+
+angleOfRightTurn = int(sys.argv[1])
+
+speed(15)
+bgcolor('black')
+r, g, b = 255, 0, 0
+
+for i in range(255*2):
+    colormode(255)
+
+    if i < 255//3:
+        g += 3
+    elif i < (255*2)//3:
+        r -= 3
+    elif i < 255:
+        b += 3
+    elif i < (255*4)//3:
+        g -= 3
+    elif i < (255*5)//3:
+        r += 3
+    else:
+        b -= 3
+
+    fd(50+i)
+    rt(angleOfRightTurn)
+    pencolor(r, g, b)
+
+# colormode(255)
+turtle.done()
+
+
+# 150, 90, 80
